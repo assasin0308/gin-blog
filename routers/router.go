879e525e7c64984cf76e57e1d1cfa00b4e07c433
shibin <shibin.bj@acewill.cn>
2024-03-06 17:04:13 +0800

    使用JWT进行身份校验

--- routers/router.go
+++ routers/router.go
@@ -17,11 +17,6 @@ func InitRouter() *gin.Engine {
 
 	gin.SetMode(setting.RunMode)
 
-	//r.GET("/test", func(c *gin.Context) {
-	//	c.JSON(200, gin.H{
-	//		"message": "test",
-	//	})
-	//})
 	r.GET("/auth", api.GetAuth)
 
 	apiv1 := r.Group("/api/v1")
