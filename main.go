a38866af8e73d51997cf36fea217f7075284b3a6
shibin <shibin.bj@acewill.cn>
2024-03-06 15:36:27 +0800

    搭建Blog APIs之 article系列:添加article及其article与tag之间的关系

--- main.go
+++ main.go
@@ -8,14 +8,8 @@ import (
 )
 
 func main() {
-	//router := gin.Default()
-	router := routers.InitRouter()
 
-	//router.GET("/test", func(c *gin.Context) {
-	//	c.JSON(200, gin.H{
-	//		"message": "test",
-	//	})
-	//})
+	router := routers.InitRouter()
 
 	s := &http.Server{
 		//Addr:           fmt.Sprintf(":%d", setting.HTTPPort),
