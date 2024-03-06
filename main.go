07c0648ab764913e3d3e7df630dc19ea5af543f4
shibin <shibin.bj@acewill.cn>
2024-03-06 14:35:35 +0800

    搭建Blog APIs之 读取.ini文件配置,链接数据库,加载基础信息

---
+++ main.go
@@ -0,0 +1,29 @@
+package main
+
+import (
+	"blog/pkg/setting"
+	"blog/routers"
+	"fmt"
+	"net/http"
+)
+
+func main() {
+	//router := gin.Default()
+	router := routers.InitRouter()
+
+	//router.GET("/test", func(c *gin.Context) {
+	//	c.JSON(200, gin.H{
+	//		"message": "test",
+	//	})
+	//})
+
+	s := &http.Server{
+		//Addr:           fmt.Sprintf(":%d", setting.HTTPPort),
+		Addr:           fmt.Sprintf(":%d", 8000),
+		Handler:        router,
+		ReadTimeout:    setting.ReadTimeout,
+		WriteTimeout:   setting.WriteTimeout,
+		MaxHeaderBytes: 1 << 20,
+	}
+	s.ListenAndServe()
+}
