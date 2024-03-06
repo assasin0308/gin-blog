531dc9c9ffe4f1360c999c0003a958617b6417ba
shibin <shibin.bj@acewill.cn>
2024-03-06 16:56:36 +0800

    使用JWT进行身份校验

--- routers/router.go
+++ routers/router.go
@@ -1,7 +1,9 @@
 package routers
 
 import (
+	"blog/middleware/jwt"
 	"blog/pkg/setting"
+	"blog/routers/api"
 	v1 "blog/routers/api/v1"
 
 	"github.com/gin-gonic/gin"
@@ -15,13 +17,15 @@ func InitRouter() *gin.Engine {
 
 	gin.SetMode(setting.RunMode)
 
-	r.GET("/test", func(c *gin.Context) {
-		c.JSON(200, gin.H{
-			"message": "test",
-		})
-	})
+	//r.GET("/test", func(c *gin.Context) {
+	//	c.JSON(200, gin.H{
+	//		"message": "test",
+	//	})
+	//})
+	r.GET("/auth", api.GetAuth)
 
 	apiv1 := r.Group("/api/v1")
+	apiv1.Use(jwt.JWT())
 	{
 		//获取标签列表
 		apiv1.GET("/tags", v1.GetTags)
